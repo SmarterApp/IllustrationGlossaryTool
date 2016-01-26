c804a466da2dc3fb13ab81035e268b769bc74ccf
shawncrabtree <crabtres@CASS-DEV12.onid.oregonstate.edu>
2016-01-26 12:00:32 -0800

    Added project structure and created dev branch

---
+++ Project/IllustrationGlossaryPackage.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllustrationGlossaryPackage.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                Console.WriteLine("Augmenting the test package: " + args[0]);
+                Console.WriteLine("Using glossary illustrations: " + args[1]);
+            }
+            Console.Read();
+        }
+    }
+}
