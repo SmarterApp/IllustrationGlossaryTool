8651ca9c3678fbf6b16093e20a087b69cd269539
Rutger Farry <rutgerfarry@gmail.com>
2016-02-10 15:39:05 -0800

    Created XElement extension method
    adds and returns a child element if it doesn't exist, otherwise just
    returns the child element

---
+++ Project/IllustrationGlossaryPackage.Core/Infrastructure/CustomExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace IllustrationGlossaryPackage.Core.Infrastructure
+{
+    public static class CustomExtensions
+    {
+        public static XElement ElementOrCreate(this XElement parent, XName name)
+        {
+            bool elementExists = parent.Elements(name).Any();
+            if (!elementExists)
+            {
+                XElement element = new XElement(name);
+                parent.Add(element);
+            }
+            return parent.Element(name);
+        }
+    }
+}
