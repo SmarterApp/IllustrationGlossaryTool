0b7a13a8182dfac37c4a5f42ab11e3f492a64011
Rutger Farry <rutgerfarry@gmail.com>
2016-02-12 15:53:21 -0800

    Extended XDocument with method to check if Content Item

--- Project/IllustrationGlossaryPackage.Dal/Infrastructure/XDocumentExtensions.cs
+++ Project/IllustrationGlossaryPackage.Dal/Infrastructure/XDocumentExtensions.cs
@@ -6,10 +6,10 @@ namespace IllustrationGlossaryPackage.Dal.Infrastructure
     public static class XDocumentExtensions
     {
         /// <summary>
-        /// Checks if the given XDocument is a Smarter Balance keyword list Xml
+        /// Checks if the given XDocument is a Smarter Balance keyword list item
         /// </summary>
         /// <param name="itemXml"></param>
-        /// <returns>True if the XDocument is a keyword list, otherwise false</returns>
+        /// <returns>True if the XDocument is a keyword list item, otherwise false</returns>
         public static bool IsKeywordListItem(this XDocument itemXml)
         {
             try
@@ -18,6 +18,26 @@ namespace IllustrationGlossaryPackage.Dal.Infrastructure
                     .Element("itemrelease")
                         .Element("item")
                             .Attribute("type").Value == "wordList";
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given XDocument is a Smarter Balance content item
+        /// </summary>
+        /// <param name="itemXml"></param>
+        /// <returns>True if the XDocument is a content item, otherwise false</returns>
+        public static bool IsContentItem(this XDocument itemXml)
+        {
+            try
+            {
+                return itemXml
+                    .Element("itemrelease")
+                        .Element("item")
+                            .Attribute("format") != null;
             }
             catch
             {
